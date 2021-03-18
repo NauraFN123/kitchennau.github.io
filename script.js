5c49f406c528b656dcd93f2e3202a9285af2775c
NauraFN123 <72368824+NauraFN123@users.noreply.github.com>
2021-03-18 21:25:32 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+window.alert("Selamat Datang di KitchenNau")
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function negaraFunction() {
+    document.getElementById("negaraDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var d = 0; d < dropdowns.length; d++) {
+      var openDropdown = dropdowns[d];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+function anekaFunction() {
+    document.getElementById("anekaDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var d = 0; d < dropdowns.length; d++) {
+      var openDropdown = dropdowns[d];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
